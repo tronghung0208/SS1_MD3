eb8fc421a1606e4f82401755cf85b4edf1b3f63c
tronghung0208 <phanhung0208ytna@gmail.com>
2023-09-19 23:08:09 +0700

    ss1_TH

---
+++ src/chuanbi/baitap1/baitap1.java
@@ -0,0 +1,12 @@
+package chuanbi.baitap1;
+
+import java.util.Date;
+
+public class baitap1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Date date = new Date();
+        System.out.println("Thời gian: " + date);
+    }
+}
